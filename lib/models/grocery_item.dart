a1d49da48b9495391a95fb89e112215847515970
shshaheen <kolimishaheen9885@gmail.com>
2025-01-29 08:24:53 +0530

    Created models and dummy_data

---
+++ lib/models/grocery_item.dart
@@ -0,0 +1,19 @@
+// import 'package:flutter/material.dart';
+import 'package:shop_list/models/category.dart';
+
+//  id: 'a',
+//       name: 'Milk',
+//       quantity: 1,
+//       category:
+class GroceryItem {
+  const GroceryItem({
+    required this.id,
+    required this.name,
+    required this.quantity,
+    required this.category
+  });
+  final String id;
+  final String name;
+  final int quantity;
+  final Category category;
+}
